4b51b161332f059afc0ce38dae791d1eba0c1902
rajdpds <rajdeepdas4490@gmail.com>
2026-03-12 20:09:40 +0530

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,24 @@
+const songs = [
+
+{
+title:"Two Week",
+artist:"FKA twigs",
+src:"audio/song1.mp3",
+cover:"cover/song1.jpg"
+},
+
+{
+title:"Higher",
+artist:"SBTRKT",
+src:"audio/song2.mp3",
+cover:"cover/song2.jpg"
+},
+
+{
+title:"2S Shuffle",
+artist:"Andrew Huang",
+src:"audio/song3.mp3",
+cover:"cover/song3.jpg"
+}
+
+]
